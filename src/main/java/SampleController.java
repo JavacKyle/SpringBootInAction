355abd14389aee1fe5d205cbc3835f7c24c65570
Kyle <xiaokai23@vip.qq.com>
2018-05-21 09:16:53 +0800

    @SpringBootApplication 开启了Spring的组件扫描和Spring Boot的自动配置功能。实际上， @SpringBootApplication 将三个有用的注解( @Configuration   @ComponentScan   @EnableAutoConfiguration)组合在了一起。

--- src/main/java/SampleController.java
+++ src/main/java/SampleController.java
@@ -1,6 +1,5 @@
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,8 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Created by Song on 2017/2/15.
  * 官方示例工程中的测试代码
  */
-@Controller
-@EnableAutoConfiguration
+//@Controller
+//@EnableAutoConfiguration
+@SpringBootApplication//@SpringBootApplication 开启了Spring的组件扫描和Spring Boot的自动配置功能。实际上， @SpringBootApplication 将三个有用的注解( @Configuration   @ComponentScan   @EnableAutoConfiguration)组合在了一起。
 public class SampleController {
     @RequestMapping("/")
     @ResponseBody
